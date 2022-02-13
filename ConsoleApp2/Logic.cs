d9fb49a79783afe5516a6e0cb95147763ad2c6ec
fixterjakes11 <rahmetovcom06@gmail.com>
2022-02-13 20:58:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Logic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratory_work1
+{
+    class Logic
+    {
+        public static void Count(int x, int y)
+        {
+            if (x < y)
+            {
+                Count(x, y - 1);
+                Console.WriteLine(y + " ");
+            }
+
+            if (x > y)
+            {
+                Count(x, y + 1);
+                Console.WriteLine(y + " ");
+            }
+        }
+
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Laboratory_work1
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Нажмите Enter для продолжения...");
+                if (Console.ReadKey().Key == ConsoleKey.Enter)
+                    Text();
+                else
+                    break;
+            }
+        }
+
+        public static void Text()
+        {
+            try
+            {
+                Console.WriteLine("Введите число A: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введите число B: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Результат: ряд чисел от A = {0} до B = {1}:", a, b);
+                Logic.Count(a, b);
+                
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка, попробуйте ввести число\n");
+            }
+        }
+    }
+
+}
